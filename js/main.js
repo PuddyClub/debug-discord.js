58c323b412c808e4185bc0a113b8cefaa77d05e5
Yasmin Seidel <jackiedevpe@outlook.com.br>
2020-04-06 09:10:34 -0300

    new files

--- js/main.js
+++ js/main.js
@@ -1,30 +1,87 @@
+const discord_manager = {
+
+    pagination: {
+        format: "[ < nnncnnn > ]",
+        onFormat: function (type) {
+
+            switch (type) {
+
+                case 'block':
+
+                    if (!this.active)
+                        return '<li class="page-item disabled"><a class="page-link" href="#">' + this.value + '</a></li>';
+                    else if (this.value != this.page)
+                        return '<li class="page-item"><a class="page-link" href="#' + this.value + '">' + this.value + '</a></li>';
+                    return '<li class="page-item active"><a class="page-link" href="#">' + this.value + '<span class="sr-only">(current)</span></a></li>';
+
+                case 'next':
+
+                    if (this.active)
+                        return '<li class="page-item"><a class="page-link" href="#' + this.value + '" aria-label="Next"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>';
+                    return '<li class="page-item disabled"><a class="page-link" href="#" aria-label="Next"><span aria-hidden="true">&raquo;</span><span class="sr-only">Next</span></a></li>';
+
+                case 'prev':
+
+                    if (this.active)
+                        return '<li class="page-item"><a class="page-link" href="#' + this.value + '" aria-label="Previous"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
+                    return '<li class="page-item disabled"><a class="page-link" href="#" aria-label="Previous"><span aria-hidden="true">&laquo;</span><span class="sr-only">Previous</span></a></li>';
+
+                case 'first':
+
+                    if (this.active)
+                        return '<li class="page-item"><a class="page-link" href="#' + this.value + '" tabindex="-1">First</a></li>';
+                    return '<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">First</a></li>';
+
+                case 'last':
+
+                    if (this.active)
+                        return '<li class="page-item"><a class="page-link" href="#' + this.value + '" tabindex="-1">Last</a></li>';
+                    return '<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Last</a></li>';
+
+                case "leap":
+
+                    if (this.active)
+                        return "   ";
+                    return "";
+
+                case 'fill':
+
+                    if (this.active)
+                        return "...";
+                    return "";
+            }
+        }
+    }
+
+};
+
 // Start System
 const startSystem = function () {
 
     // Load
     $.LoadingOverlay("show", { background: "rgba(0,0,0, 0.5)" });
-    const bot = new Discord.Client({ autoReconnect: true });
+    discord_manager.bot = new Discord.Client({ autoReconnect: true });
 
     // Ready
-    bot.on('ready', (event) => {
+    discord_manager.bot.on('ready', (event) => {
 
-        console.log(`Discord Logged in as ${bot.user.tag}!`);
+        console.log(`Discord Logged in as ${discord_manager.bot.user.tag}!`);
 
     });
 
     // Reconnect
-    bot.on('disconnect', (erMsg, code) => {
+    discord_manager.bot.on('disconnect', (erMsg, code) => {
         console.error(`${erMsg}! Code: ${code}`);
     });
 
     // Error
 
-    bot.on('error', (erMsg) => {
+    discord_manager.bot.on('error', (erMsg) => {
         console.error(erMsg);
     });
 
     // Warn
-    bot.on('warn', (erMsg) => {
+    discord_manager.bot.on('warn', (erMsg) => {
         console.warn(erMsg);
     });
 
@@ -38,7 +95,7 @@ const startSystem = function () {
     }
 
     // Login
-    bot.login($("#discord_token").val()).then(function () {
+    discord_manager.bot.login($("#discord_token").val()).then(function () {
 
         // Start Page
         $.LoadingOverlay("hide");
@@ -58,11 +115,40 @@ const startSystem = function () {
                     // Menu
                     $("<div>", { class: "collapse navbar-collapse", id: "navbarSupportedContent" }).append(
 
-                        
+                        // Left
+                        $("<ul>", { class: "navbar-nav mr-auto mt-2 mt-lg-0" }).append(
+
+                            // Server List
+                            $("<li>", { class: "nav-item" }).append(
+                                $("<a>", { class: "nav-link", href: "#" }).text('Server List')
+                            ).click(function () {
+                                discord_manager.server_list(1);
+                            }),
+
+                            // Debug List
+                            $("<li>", { class: "nav-item" }).append(
+                                $("<a>", { class: "nav-link", href: "#" }).text('Debug')
+                            )
+
+                        ),
+
+                        // Right
+                        $("<ul>", { class: "nav navbar-nav navbar-right ml-3" }).append(
+
+                            // Patreon
+                            $("<li>", { class: "nav-item" }).append(
+                                $("<a>", { class: "btn btn-info", target: "_blank", href: "https://www.patreon.com/JasminDreasond" }).append(
+                                    $("<i>", { class: "fab fa-patreon" }), ' Patreon'
+                                )
+                            )
+
+                        )
 
                     )
 
-                )
+                ),
+
+                $("<div>", { id: "app_base" })
 
             );
 
---
+++ js/server_list.js
@@ -0,0 +1,28 @@
+discord_manager.server_list = function (page) {
+
+    const items = Array.from(discord_manager.bot.guilds.cache.keys());
+    console.log(items);
+    items.push(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24);
+
+    const pagination = $("<ul>", { class: "pagination" });
+
+    $('#app_base').empty().append(pagination);
+    pagination.paging(1000, {
+
+        format: discord_manager.pagination.format,
+        perpage: 20,
+        lapping: 1,
+        page: page,
+        onSelect: function (page) {
+
+            this.slice[0] = this.slice[0] + Number(page - 1);
+            this.slice[1] = this.slice[1] + Number(page - 1);
+            for (let i = this.slice[0]; i < this.slice[1]; i++) {
+                console.log(items[i]);
+            }
+
+        },
+        onFormat: discord_manager.pagination.onFormat
+    });
+
+};
